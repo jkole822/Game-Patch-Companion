99e926667a149ef6cd21577d089bedf20b98279a
Kole Gasior <jkole822@gmail.com>
2026-05-11 10:37:20 -0400

    Update projectId in Sanity CLI configuration and add deployment script to package.json

--- apps/cms/sanity.cli.ts
+++ apps/cms/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: '21ubekcu',
+    projectId: 'un21b1thb3kpzcfhvmftwibf',
     dataset: 'production',
   },
   deployment: {
