5e0e7c8aa5408532ea695b1fe8e472af2276f626
Kole Gasior <jkole822@gmail.com>
2026-05-11 08:37:30 -0400

    Refactor navigation component for improved structure and styling

--- apps/web/app/components/foundations/Navigation/Navigation.tsx
+++ apps/web/app/components/foundations/Navigation/Navigation.tsx
@@ -19,79 +19,87 @@ export const Navigation = ({ cta, links, logoutAction }: NavigationProps) => {
     ) || [];
 
   return (
-    <nav className="navigation page-margins">
-      <NextLink className="navigation__home" href="/" title="Home">
-        <Image
-          alt="Hearthstone"
-          className="navigation__home-image"
-          height={1024}
-          preload
-          src="/gpc-hearthstone.png"
-          width={1024}
-        />
-      </NextLink>
-      <ul className="navigation__link-list">
-        {filteredLinks.map(({ label, href }) => (
-          <li key={label}>
-            <Link href={href}>{label}</Link>
-          </li>
-        ))}
-        {cta && (
-          <li>
-            <Button href={cta.href}>{cta.label}</Button>
-          </li>
-        )}
-        {logoutAction && (
-          <li>
-            <form action={logoutAction} method="post">
-              <Button type="submit">Log out</Button>
-            </form>
-          </li>
-        )}
-      </ul>
-      <Drawer
-        hideHeader
-        open={mobileNavigationOpen}
-        onOpenChange={setMobileNavigationOpen}
-        title="Menu"
-        trigger={
-          <button className="navigation__mobile-menu-button group">
-            <div className="navigation__mobile-menu-icon">
-              <svg viewBox="0 0 32 32">
-                <path
-                  className="navigation__mobile-menu-icon-path navigation__mobile-menu-icon-path--top-bottom"
-                  d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
-                ></path>
-                <path className="navigation__mobile-menu-icon-path" d="M7 16 27 16"></path>
-              </svg>
-            </div>
-          </button>
-        }
-      >
-        <ul className="navigation__link-list navigation__link-list--mobile">
+    <nav className="navigation">
+      <div className="navigation__content page-margins">
+        <NextLink className="navigation__home" href="/" title="Home">
+          <Image
+            alt="Hearthstone"
+            className="navigation__home-image"
+            height={1024}
+            preload
+            src="/gpc-hearthstone.png"
+            width={1024}
+          />
+        </NextLink>
+        <ul className="navigation__link-list">
           {filteredLinks.map(({ label, href }) => (
-            <li className="navigation__link-item navigation__link-item--mobile" key={label}>
+            <li key={label}>
               <Link href={href}>{label}</Link>
             </li>
           ))}
           {cta && (
             <li>
-              <Button className="navigation__link-cta navigation__link-cta--mobile" href={cta.href}>
-                {cta.label}
-              </Button>
+              <Button href={cta.href}>{cta.label}</Button>
             </li>
           )}
           {logoutAction && (
             <li>
               <form action={logoutAction} method="post">
-                <Button className="navigation__link-cta navigation__link-cta--mobile" type="submit">
-                  Log out
-                </Button>
+                <Button type="submit">Log out</Button>
               </form>
             </li>
           )}
         </ul>
-      </Drawer>
+        <Drawer
+          hideHeader
+          open={mobileNavigationOpen}
+          onOpenChange={setMobileNavigationOpen}
+          title="Menu"
+          trigger={
+            <button className="navigation__mobile-menu-button group">
+              <div className="navigation__mobile-menu-icon">
+                <svg viewBox="0 0 32 32">
+                  <path
+                    className="navigation__mobile-menu-icon-path navigation__mobile-menu-icon-path--top-bottom"
+                    d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
+                  ></path>
+                  <path className="navigation__mobile-menu-icon-path" d="M7 16 27 16"></path>
+                </svg>
+              </div>
+            </button>
+          }
+        >
+          <ul className="navigation__link-list navigation__link-list--mobile">
+            {filteredLinks.map(({ label, href }) => (
+              <li className="navigation__link-item navigation__link-item--mobile" key={label}>
+                <Link href={href}>{label}</Link>
+              </li>
+            ))}
+            {cta && (
+              <li>
+                <Button
+                  className="navigation__link-cta navigation__link-cta--mobile"
+                  href={cta.href}
+                >
+                  {cta.label}
+                </Button>
+              </li>
+            )}
+            {logoutAction && (
+              <li>
+                <form action={logoutAction} method="post">
+                  <Button
+                    className="navigation__link-cta navigation__link-cta--mobile"
+                    type="submit"
+                  >
+                    Log out
+                  </Button>
+                </form>
+              </li>
+            )}
+          </ul>
+        </Drawer>
+      </div>
     </nav>
   );
 };
